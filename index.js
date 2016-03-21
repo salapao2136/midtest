8a0d89cc0fa514cc39eef7fd8bac108709361c65
kingkong64 <kingkong2103@gmail.com>
2016-03-22 00:10:47 +0700

    heroku serv

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static('public'))
+app.set('port', (process.env.PORT || 5000))
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.use(express.static(__dirname + '/public'))
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
 })
